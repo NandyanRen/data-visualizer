c552b008e8dfe03ce67de7834b69b92e1e8478be
DESKTOP-MJKU67N\johnr <johnrencveluz@gmail.com>
2022-09-11 00:34:01 +0800

    code cleanup and formatted x and y axis

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -11,13 +11,9 @@
         <div
           v-for="profile in profiles"
           :key="profile">
-              <div>
                   <h2>{{profile.name}}</h2>
-              </div>
-              <div>
                   <p>{{profile.value +' MWh'}}</p>
                   <hr />
-              </div>
             </div>
       </div>
       <div class="chartContainer">
@@ -31,7 +27,6 @@
 import Chart from 'chart.js/auto';
 import sampleData from '../data/sample-dataset.json'
 import profiles from '../data/load-profiles.json'
-
 
 export default {
   name: 'Dashboard',
@@ -48,13 +43,14 @@ export default {
   },
   
   methods: {
-    getDataset(data, color) { 
+    getDataset(data, color, lineWidth) { 
       return { 
         data: data,
         fill: false,
         borderColor: color,
         tension: 0.1,
         pointRadius: 0,
+        borderWidth: lineWidth,
       };
     },
     getAverage(i, days) {
@@ -63,13 +59,22 @@ export default {
         sum += day.values[i];
       });
       return sum / days.length;
-    }
+    },
+    timeConvert(n) {
+      const num = n;
+      const hours = (num / 60);
+      const rhours = Math.floor(hours);
+      const minutes = (hours - rhours) * 60;
+      const rminutes = Math.round(minutes);
+      return rhours + ':' + rminutes;
+      }
   },
 
   mounted(){
     let i = 0;
     let lineChartData = [];
     let dataAverage = [];
+    
     const ctx = document.getElementById('myChart');
 
     // ## Missing Time Format For X-Axis label
@@ -81,10 +86,10 @@ export default {
       dataAverage.push(this.getAverage(i, this.days));
     }
 
-    lineChartData.push(this.getDataset(dataAverage, 'rgb(16,45,76)'));
+    lineChartData.push(this.getDataset(dataAverage, 'rgb(16,45,76)', 5));
 
     this.days.forEach(day => {
-      lineChartData.push(this.getDataset(day.values, 'rgb(198,198,198)'));
+      lineChartData.push(this.getDataset(day.values, 'rgb(198,198,198)', .2));
     });
 
     const data = {
@@ -98,16 +103,38 @@ const myChart = new Chart(ctx, {
   options: {
       plugins: {
          legend: {
-            display: false
+            display: false,
          }
       },
       scales: {
-      x: {
-        grid: {
-            display: false
+        x: {
+          grid: {
+              display: false
+              },
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 10,
+            callback: function(value){
+              let hours, counter = 0;
+              const xLabel = parseInt(this.getLabelForValue(value));
+              counter = xLabel / 30;
+              hours = counter / 2;
+              if(counter % 2 != 0){
+                return Math.floor(hours) + ':' + '30';
+              }
+              return hours + ':' + '00';
             }
+          }
           },
-        }
+        y: {
+          ticks: {
+            callback: function(value){
+              const formatter = Intl.NumberFormat('en', { notation: 'compact'})
+              return formatter.format(parseInt(this.getLabelForValue(value))) + 'k';
+            },
+          }
+        },
+      }
       }
   });
 
